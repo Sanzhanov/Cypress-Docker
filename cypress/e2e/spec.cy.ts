2b59311f7e7d708cee78962e2d4ea72561a857cd
Sanzhanov <sanzhanov21@gmail.com>
2023-05-06 00:22:03 -0400

    Test changed

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,17 +1,27 @@
 describe('template spec', () => {
+
   beforeEach(() => {
     cy.visit('/testing-with-cypress')
   });
 
   it('Verify header', () => {
-    cy.get('h1').should('be.visible').and('contains.text', 'Testing with Cypress')
+    cy
+      .get('h1')
+      .should('be.visible')
+      .and('contains.text', 'Testing with Cypress')
   })
 
   it('Verify slogan', () => {
-    cy.get('h2').should('be.visible').and('contains.text', 'Test smarter, not harder with Cypress.')
+    cy
+      .get('h2')
+      .should('be.visible')
+      .and('contains.text', 'Test smarter, not harder with Cypress.')
   })
 
   it('Verify button set', () => {
-    cy.get('.buttonSet li').should('be.visible').and('have.length', 3)
+    cy
+      .get('.buttonSet li')
+      .should('be.visible')
+      .and('have.length', 3)
   })
 })
