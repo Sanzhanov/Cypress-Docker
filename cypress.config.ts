5e6f235b3aae3ad8a472b444a47422a08bc143e2
Sanzhanov <sanzhanov21@gmail.com>
2023-05-21 01:59:36 -0400

    Cypress config changed

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
   screenshotOnRunFailure: true,
-  videoUploadOnPasses: false,
+  videoUploadOnPasses: true,
   video: true,
   videoCompression: false
 })
