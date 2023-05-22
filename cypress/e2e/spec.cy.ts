a6add6dd70168053bda71976e88433f13ab34d3f
Sanzhanov <sanzhanov21@gmail.com>
2023-05-21 23:50:05 -0400

    Tests changed

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -5,21 +5,21 @@ describe('Testing the homepage of the blog Testing with Cypress', () => {
       .visit('http://medium.com/testing-with-cypress')
   });
 
-  it('Verify header', () => {
+  it('header should contain specific text', () => {
     cy
       .get('h1')
       .should('be.visible')
       .and('contains.text', 'Testing with Cypress')
   })
 
-  it('Verify slogan', () => {
+  it('slogan should contain specific text', () => {
     cy
       .get('h2')
       .should('be.visible')
       .and('contains.text', 'Test smarter, not harder with Cypress.')
   })
 
-  it('Verify buttons set', () => {
+  it('button set should include three buttons', () => {
     cy
       .get('.buttonSet li')
       .should('be.visible')
